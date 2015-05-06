054ec3f3aa184dc90afb8a7fc78b94800aff1fc0
Nicolas Noé <nicolas@niconoe.eu>
2015-05-06 11:39:12 +0200

    Main model (EPUIndexScore) created.

--- epu_index/models.py
+++ epu_index/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class EpuIndexScore(models.Model):
+    date = models.DateField()
+    number_of_articles = models.IntegerField()
+    number_of_papers = models.IntegerField()
+    epu = models.FloatField()
