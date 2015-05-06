d23d8e750b5e8473896b42c46490f61a23384582
Nicolas Noé <nicolas@niconoe.eu>
2015-05-06 14:30:34 +0200

    Implemented REST view (#2).

--- epu_index/views.py
+++ epu_index/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import EpuIndexScore
+from .serializers import EpuSerializer
+
+
+class EpuViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = EpuIndexScore.objects.all()
+    serializer_class = EpuSerializer
