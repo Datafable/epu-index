258d049e5f026cafa74b5a13da4afdcc8a581ae9
Nicolas Noé <nicolas@niconoe.eu>
2015-05-18 14:42:45 +0200

    Implemented Article model, fixes #7.

--- epu_index/models.py
+++ epu_index/models.py
@@ -6,3 +6,13 @@ class EpuIndexScore(models.Model):
     number_of_articles = models.IntegerField()
     number_of_papers = models.IntegerField()
     epu = models.FloatField()
+
+
+class Article(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    published_at = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.title
