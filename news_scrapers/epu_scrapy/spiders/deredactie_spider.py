4c3f1e7ec9d616ec4bbb3a7548cc89515cf0eb25
Bart Aelterman <bart.aelterman@gmail.com>
2015-05-26 17:30:28 +0200

    fix deredactie spider

--- news_scrapers/epu_scrapy/spiders/deredactie_spider.py
+++ news_scrapers/epu_scrapy/spiders/deredactie_spider.py
@@ -8,10 +8,11 @@ import re
 
 class DeredactieSpider(CrawlSpider):
     name = 'deredactie' # name of the spider, to be used when running from command line
-    allowed_domains = ['www.deredactie.be']
+    allowed_domains = ['deredactie.be']
     today = datetime.today()
     search_day = today - timedelta(days=1) # search for articles of yesterday
     search_day_str = '{0}/{1}/{2}'.format(today.day, today.month, today.year % 100)
+    pagesize = 20
     start_urls = ['http://deredactie.be/cm/vrtnieuws/1.516538?text=economie&type=text&range=atdate&isdate={0}&sort=date&action=submit&advancedsearch=on'.format(search_day_str)]
 
     def parse(self, response):
@@ -21,20 +22,20 @@ class DeredactieSpider(CrawlSpider):
         parse_list_page function
         """
         nr_of_articles_element = response.xpath('//li[contains(concat(" ", normalize-space(@class), " "), " searchcounter ")]/text()').extract()
-        if len(nr_of_articles_element) is 1:
+        if len(nr_of_articles_element) is 2:
+            # nr of articles is mentioned above list of articles and below. So the number of elements that match the xpath selector is 2
             nr_of_articles = int(re.findall('[0-9]+', nr_of_articles_element[0].encode('utf-8'))[0])
-            for i in range(0, nr_of_articles, 20):
+            for i in range(0, nr_of_articles, self.pagesize):
                 yield scrapy.Request(self.start_urls[0] + '&offset={0}'.format(i), callback=self.parse_list_page)
         else:
-            raise scrapy.CloseSpider("could not parse number of articles from {0}".format(response.url))
-
+            raise scrapy.exceptions.CloseSpider("could not parse number of articles from {0}".format(response.url))
 
 
     def parse_published_datetime(self, datetime_element_parts):
         """
         Helper method to parse a datetime from a html element
         """
-        datetime_str_parts = [x.encode('utf-8') for x in datetime_element_parts.xpath('text()').extract()]
+        datetime_str_parts = [x.encode('utf-8') for x in datetime_element_parts]
         datetime_str = ' '.join(datetime_str_parts).strip()
         datetime_str_stripped = re.findall('[0-9]+/[0-9]+/[0-9]+[^0-9]+[0-9]+:[0-9]+', datetime_str)[0]
         dt = strptime(datetime_str_stripped, '%d/%m/%Y - %H:%M')
@@ -48,7 +49,6 @@ class DeredactieSpider(CrawlSpider):
         scrapy.Request objects for every link found. The parsing of these links is done by the parse_article
         function.
         """
-        print 'CUSTOM OUTPUT: page: {0}'.format(self.page)
         print response.url
         links = response.xpath('//div[contains(concat(" ", normalize-space(@class), " "), " searchresults ")]/descendant::a/@href').extract()
         link_set = set([x.encode('utf-8') for x in links])
@@ -60,11 +60,11 @@ class DeredactieSpider(CrawlSpider):
                 # single article page, the url is /cm/vrtnieuws/binnenland/<articleid>. Both a storylineid and a articleid
                 # look something like 1.193019, which will be matched by the regular expression pattern [0-9.]+
                 article_id = re.findall('[0-9.]+', l)[-1] # the last id in the url is the article id
-                l = 'http://deredactie.be/cm/' + l
+                l = 'http://deredactie.be/cm/' + article_id
                 yield scrapy.Request(l, callback=self.parse_article)
 
 
-    def parse_article(self, response, article_id):
+    def parse_article(self, response):
         """
         Parse the article content page
         """
@@ -88,8 +88,8 @@ class DeredactieSpider(CrawlSpider):
         article_intro = ' '.join([x.strip().encode('utf-8') for x in article_intro_parts]).strip()
 
         # search for article full text
-        article_full_text_fragments = response.xpath('//div[@id="articlebody"]/descendant::p/text()').extract()
-        article_full_text = '\n'.join([x.strip().encode('utf-8') for x in article_full_text_fragments]).strip()
+        article_full_text_fragments = response.xpath('//div[@id="articlebody"]/descendant::p/descendant-or-self::*/text()').extract()
+        article_full_text = ' '.join([x.strip().encode('utf-8') for x in article_full_text_fragments]).strip()
 
         # now create an Article item, and return it. All Articles created during scraping can be written to an output file when the -o option is given.
         article = Article()
