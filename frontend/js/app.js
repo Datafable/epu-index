aab7013dee3e72c0e893179ec2ace072ef520cc2
Peter Desmet <peter.desmet.work@gmail.com>
2015-07-06 14:40:15 +0200

    Layout x and y-axis
    * Don’t wrap in one createChart function (=> better access to helper
    functions)
    * x-axis ticks: one per year (using values parameter)
    * y-axis: hide

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,78 +1,99 @@
 var app = function() {
-    var createAndPopulateOverviewChart = function() {
-        // var endpoint = "https://epu-index.herokuapp.com/api/epu/?format=json&start=2013-01-01&end=2013-02-01",
-        var endpoint = "http://bartaelterman.cartodb.com/api/v2/sql?q=SELECT (sum(number_of_articles)::real / sum(number_of_newspapers)::real) as epu, to_char(date, 'YYYY-MM') as date FROM epu_tail GROUP BY  to_char(date, 'YYYY-MM') ORDER BY to_char(date, 'YYYY-MM')";
 
-        d3.json(endpoint, function(d) {
-            var months = d.rows.map(function(e) { return new Date(e.date); }), // Remove "rows. for final endpoint"
-                epu = d.rows.map(function(e) { return e.epu; }), // Remove "rows. for final endpoint",
-                formatTooltipTitle = d3.time.format("%Y-%m"),
-                showYearRegion = function(month) {
-                    // Given a month (full Date object), get dates 6 monts before and after.
-                    var startMonth = new Date(new Date(month).setMonth(month.getMonth()-6)),
-                        endMonth = new Date(new Date(month).setMonth(month.getMonth()+6))
+    // Chart layout functions
+    var formatAsYearMonth = d3.time.format("%Y-%m");
+    var createTickSeries = function(extent,aggregateBy) {
+        // Create an array of dates for axis ticks, by year or month e.g. 2001-01-01, 2002-01-01
+        var min = new Date(extent[0]),
+            max = new Date(extent[1]),
+            ticks = [];
 
-                    console.log(month + ":" + startMonth + " " + endMonth);
-                    overviewChart.regions.remove()
-                    overviewChart.regions.add([
-                        { axis: "x", start: startMonth, end: endMonth } 
-                    ]);
-                };
+        if (aggregateBy == "year") {
+            for (year = min.getUTCFullYear(); year <= max.getUTCFullYear(); year++) {
+                ticks.push(new Date(year + "-01-01"));
+            }
+        }
+        return ticks;
+    }
 
-            var overviewChart = c3.generate({
-                    axis: {
-                        x: {
-                            localtime: false,
-                            padding: {
-                                left: 0,
-                                right: 0
-                            },
-                            tick: {
-                                // count: numberOfYears,
-                                culling: {
-                                    // max: numberOfYears
-                                },
-                                format: "%Y-%m"
-                            },
-                            type: "timeseries"
-                        },
-                        y: {
-                            show: false
-                        }
+    var showYearRegion = function(month) {
+            // Given a month (full Date object), get dates 6 months before and after.
+            var startMonth = new Date(new Date(month).setMonth(month.getMonth()-6)),
+                endMonth = new Date(new Date(month).setMonth(month.getMonth()+6))
+
+            console.log(month + ":" + startMonth + " " + endMonth);
+            overviewChart.regions.remove()
+            overviewChart.regions.add([
+                { axis: "x", start: startMonth, end: endMonth } 
+            ]);
+        };
+
+    
+    // Create overview chart
+    var epuPerMonthData = "http://bartaelterman.cartodb.com/api/v2/sql?q=SELECT (sum(number_of_articles)::real / sum(number_of_newspapers)::real) as epu, to_char(date, 'YYYY-MM') as date FROM epu_tail GROUP BY to_char(date, 'YYYY-MM') ORDER BY to_char(date, 'YYYY-MM')";
+    d3.json(epuPerMonthData, function(d) {
+        var months = d.rows.map(function(e) { return new Date(e.date); }), // Remove "rows. for final endpoint"
+            epu = d.rows.map(function(e) { return e.epu; }); // Remove "rows. for final endpoint"
+            formatTooltipTitle = d3.time.format("%Y-%m");
+
+        var overviewChart = c3.generate({
+            axis: {
+                x: {
+                    localtime: false,
+                    padding: {
+                        left: 0,
+                        right: 0
                     },
-                    bindto: "#overview-chart",
-                    data: {
-                        colors: {
-                            "epu": "black"
-                        },
-                        columns: [
-                            ["months"].concat(months),
-                            ["epu"].concat(epu)
-                        ],
-                        onclick: function (d) { showYearRegion(d.x); },
-                        type: "area-spline",
-                        x: "months"
+                    tick: {
+                        values: createTickSeries(d3.extent(months),"year"),
+                        format: "%Y"
                     },
-                    legend: {
-                        show: false
-                    },
-                    point: {
-                        show: false
-                    },
-                    size: {
-                        height: 100
-                    },
-                    tooltip: {
-                        // show: false
-                        format: {
-                            title: function (d) { return formatTooltipTitle(d); }
-                        }
-                    }
-                });
+                    type: "timeseries"
+                },
+                y: {
+                    show: false
+                }
+            },
+            bindto: "#overview-chart",
+            data: {
+                colors: {
+                    "epu": "black"
+                },
+                columns: [
+                    ["months"].concat(months),
+                    ["epu"].concat(epu)
+                ],
+                // onclick: function (d) { showYearRegion(d.x); },
+                type: "area-spline",
+                x: "months"
+            },
+            legend: {
+                show: false
+            },
+            padding: {
+                left: 20,
+                right: 20
+            },
+            point: {
+                show: false
+            },
+            size: {
+                height: 100
+            },
+            tooltip: {
+                // show: false
+                format: {
+                    title: function (d) { return formatAsYearMonth(d); }
+                }
+            }
         });
-    };
-    createAndPopulateOverviewChart();
+    });
 }();
+
+
+
+
+
 
 var words = d3.json("http://bartaelterman.cartodb.com/api/v2/sql?q=select text,count from term_frequencies limit 30", function(d) {
     var fill = d3.scale.category20(); // TODO: create custom color schema
