1e24b9e98cc7a2557b25fb6371c07c9c43d3d3d4
Nicolas Noé <nicolas@niconoe.eu>
2015-07-10 10:59:37 +0200

    New service to provide EPU per month. Implements #39.

--- webapp/epu_index/views.py
+++ webapp/epu_index/views.py
@@ -5,7 +5,11 @@ from datetime import datetime
 from django.http import StreamingHttpResponse
 from django.db import models
 
+from rest_framework.decorators import api_view, permission_classes
+
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from .models import EpuIndexScore
 from .serializers import EpuSerializer
@@ -62,28 +66,58 @@ def epu_streaming_csv(request):
     return response
 
 
+def _filterdates_epu_queryset(start_date, end_date):
+    def _param_to_date(string):
+            """ Takes a YYYY-MM-DD string and return a Date object."""
+            return datetime.strptime(string, '%Y-%m-%d').date()
+
+    queryset = EpuIndexScore.objects.all()
+
+    # Optional filtering by start/end date.
+    # Params are named 'start' and 'end' and use YYYY-MM-DD format.
+    # Filters are inclusive (<= / >=)
+    if start_date is not None:
+        queryset = queryset.filter(date__gte=_param_to_date(start_date))
+
+    if end_date is not None:
+        queryset = queryset.filter(date__lte=_param_to_date(end_date))
+
+    return queryset
+
+
+@api_view()
+@permission_classes((AllowAny, ))
+def epu_per_month(request):
+    start_date = request.GET.get('start', None)
+    end_date = request.GET.get('end', None)
+
+    queryset = _filterdates_epu_queryset(start_date, end_date)
+
+    # 1. For each year-month pair, we keep the sum of EPUs and the number of involved days
+    months = {}
+    for e in queryset:
+        key = "{year}-{month:0>2d}".format(year=e.date.year, month=e.date.month)
+        if key in months:
+            months[key]['total_epu'] += e.epu
+            months[key]['days_count'] += 1
+        else:
+            months[key] = {'total_epu': e.epu, 'days_count': 1}
+
+    # 2. Based on the sum and number of days, we compute an average per month:
+    processed_months = {}
+    for k, v in months.iteritems():
+        processed_months[k] = v['total_epu'] / v['days_count']
+
+    return Response(processed_months)
+
+
 class EpuViewSet(viewsets.ReadOnlyModelViewSet):
     base_name = 'epu'
     queryset = EpuIndexScore.objects.all()
     serializer_class = EpuSerializer
 
     def get_queryset(self):
-        def _param_to_date(string):
-            """ Takes a YYYY-MM-DD string and return a Date object."""
-            return datetime.strptime(string, '%Y-%m-%d').date()
-
-        queryset = EpuIndexScore.objects.all()
-
-        # Optional filtering by start/end date.
-        # Params are named 'start' and 'end' and use YYYY-MM-DD format.
-        # Filters are inclusive (<= / >=)
         start_date = self.request.query_params.get('start', None)
         end_date = self.request.query_params.get('end', None)
 
-        if start_date is not None:
-            queryset = queryset.filter(date__gte=_param_to_date(start_date))
-
-        if end_date is not None:
-            queryset = queryset.filter(date__lte=_param_to_date(end_date))
-
-        return queryset
+        return _filterdates_epu_queryset(start_date, end_date)
