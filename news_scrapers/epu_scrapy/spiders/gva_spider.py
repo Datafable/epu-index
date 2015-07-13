ed4dd17bf1f94b612a657c210ac49df6ade490af
Bart Aelterman <bart.aelterman@gmail.com>
2015-07-13 16:29:48 +0200

    gva: datetime format

--- news_scrapers/epu_scrapy/spiders/gva_spider.py
+++ news_scrapers/epu_scrapy/spiders/gva_spider.py
@@ -51,7 +51,8 @@ class GazetVanAntwerpenSpider(CrawlSpider):
         # search for article published date
         datetime_str_parts = response.xpath('//article/div[2]/div/div/footer/p/time/@datetime').extract()
         if len(datetime_str_parts) > 0:
-            datetime_str = datetime_str_parts[0].encode('utf-8')
+            dt = datetime(*strptime(datetime_str_parts[0].encode('utf-8').split('+')[0], '%Y-%m-%d %H:%M')[0:6])
+            datetime_str = dt.isoformat()
         else:
             datetime_str = ''
 
