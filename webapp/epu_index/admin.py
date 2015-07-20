4996f1f7617ec7f6f86cbb5b230a2d17819ee219
bartaelterman <bart.aelterman@gmail.com>
2015-07-20 17:12:27 +0200

    configure admin interface

--- webapp/epu_index/admin.py
+++ webapp/epu_index/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import EpuIndexScore, Article, NewsJournal
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_filter = ('news_journal',)
+    date_hierarchy= 'published_at'
+    list_display = ('title', 'published_at', 'news_journal')
+
+
 admin.site.register(EpuIndexScore)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(NewsJournal)
