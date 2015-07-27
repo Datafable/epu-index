8aafb0c1739b565bb774bc70bfef92b1e396ebe2
Nicolas Noé <nicolas@niconoe.eu>
2015-07-27 16:03:09 +0200

    Removed old, duplicate folder. Fixes #59.

--- epu_index/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import EpuIndexScore, Article
-
-admin.site.register(EpuIndexScore)
-admin.site.register(Article)
--- epu_index/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import EpuIndexScore
-from rest_framework import serializers
-
-
-class EpuSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = EpuIndexScore
-        fields = ('date', 'epu')
