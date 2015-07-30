9df1fd085f415ccdfb7afe9b89b9d848ae64c940
bartaelterman <bart.aelterman@gmail.com>
2015-07-30 11:41:05 +0200

    fix #60

--- webapp/epu_index/admin.py
+++ webapp/epu_index/admin.py
@@ -5,7 +5,7 @@ from .models import EpuIndexScore, Article, NewsJournal
 class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('news_journal',)
     date_hierarchy= 'published_at'
-    list_display = ('title', 'published_at', 'news_journal')
+    list_display = ('title', 'published_at', 'news_journal', 'epu_score')
 
 
 admin.site.register(EpuIndexScore)
