c2cf11e77a5ef36b9fe6e706013330815047ac51
Peter Desmet <peter.desmet.work@gmail.com>
2015-08-03 17:37:19 +0200

    Reactivate word cloud code

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -226,6 +226,43 @@ var app = (function() {
         });
     };
 
+    var wordcloud = d3.json("http://bartaelterman.cartodb.com/api/v2/sql?q=select text,count from term_frequencies limit 30", function(d) {
+        var fill = d3.scale.category20(); // TODO: create custom color schema
+        var scalingFactor = 7; // TODO: should be determined based on input counts
+
+        var w = d.rows.map(function(f) {
+            return {text: f.text, size: 10 + f.count * scalingFactor};
+        });
+
+        d3.layout.cloud().size([300, 300])
+            .words(w)
+            .padding(0)
+            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .font("Georgia")
+            .fontSize(function(d) { return d.size; })
+            .on("end", draw)
+            .start();
+
+        function draw(words) {
+            d3.select("#word-cloud").append("svg")
+                .attr("width", 300)
+                .attr("height", 300)
+                .append("g")
+                .attr("transform", "translate(150,150)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("font-family", "Georgia")
+                .style("fill", function(d, i) { return fill(i); })
+                .attr("text-anchor", "middle")
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+        }
+    });
+
 
     // Get EPU data per month, derive date range and create charts
     d3.json("https://epu-index.herokuapp.com/api/epu-per-month/?format=json", function(d) {
@@ -242,43 +279,3 @@ var app = (function() {
         createOverviewChart(d);
     });
 })();
-
-
-
-/*
-var words = d3.json("http://bartaelterman.cartodb.com/api/v2/sql?q=select text,count from term_frequencies limit 30", function(d) {
-    var fill = d3.scale.category20(); // TODO: create custom color schema
-    var scalingFactor = 7; // TODO: should be determined based on input counts
-
-    var w = d.rows.map(function(f) {
-        return {text: f.text, size: 10 + f.count * scalingFactor};
-    });
-    d3.layout.cloud().size([300, 300])
-        .words(w)
-        .padding(0)
-        .rotate(function() { return ~~(Math.random() * 2) * 90; })
-        .font("Georgia")
-        .fontSize(function(d) { return d.size; })
-        .on("end", draw)
-        .start();
-
-    function draw(words) {
-        d3.select("#word-cloud").append("svg")
-            .attr("width", 300)
-            .attr("height", 300)
-            .append("g")
-            .attr("transform", "translate(150,150)")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("font-family", "Georgia")
-            .style("fill", function(d, i) { return fill(i); })
-            .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) { return d.text; });
-    }
-});
-*/
