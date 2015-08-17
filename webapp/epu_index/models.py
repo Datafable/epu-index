c8c50972e48ea3b4acb278d2831c5466397c7b2b
Nicolas Noé <nicolas@niconoe.eu>
2015-08-17 12:50:11 +0200

    Added JournalsScraped model.

--- webapp/epu_index/models.py
+++ webapp/epu_index/models.py
@@ -23,6 +23,13 @@ class NewsJournal(models.Model):
         return self.name
 
 
+# This table/model stores a log of which scrapers successfully ran on a give day.
+# It provides necessary data to calculate the EPU index of a given day. See issue #61 and #64
+class JournalsScraped(models.Model):
+    journal = models.ForeignKey(NewsJournal)
+    date = models.DateField()
+
+
 class Article(models.Model):
     news_journal = models.ForeignKey(NewsJournal, null=True)
     intro = models.TextField(blank=True, null=True)
