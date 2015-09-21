43b593cf2699a8d07e27d7340ea1f6327717c3ae
Nicolas Noé <nicolas@niconoe.eu>
2015-09-21 10:47:46 +0200

    calculate_all_words_per_day now use pagination to avoid eating all memory.

--- webapp/epu_index/management/commands/calculate_all_words_per_day.py
+++ webapp/epu_index/management/commands/calculate_all_words_per_day.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 
 from epu_index.models import (Article, WordsPerDay, create_wordsperday_from_articles,
                               ARTICLES_APU_CUTOFF)
@@ -11,11 +12,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Calculate word frequency for all articles...")
 
-        # Only EPU positive articles are taken into account:
-        articles = Article.objects.filter(epu_score__gt=ARTICLES_APU_CUTOFF)
-
         # First we have to truncate WordsPerDay
         self.stdout.write("Truncate previous data...")
         WordsPerDay.objects.all().delete()
+
         self.stdout.write("Calculate new word frequency. That may takes long.")
-        create_wordsperday_from_articles(articles)
+
+        # We use a paginator to avoid loading all articles into memory
+        paginator = Paginator(Article.objects.filter(epu_score__gt=ARTICLES_APU_CUTOFF), 1000)
+
+        for page in range(1, paginator.num_pages + 1):
+            create_wordsperday_from_articles(paginator.page(page).object_list)
+            self.stdout.write('.')
+            self.stdout.flush()
